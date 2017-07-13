3da1e797ec70aae563ab0017b0c3587be60f67d3
JIN-DAI <daijing491@gmail.com>
2017-07-13 16:24:25 +0200

    use template of Utils.py from Danijar

--- Utils.py
+++ Utils.py
@@ -1,25 +1,39 @@
-#!/usr/bin/env python
-#coding=utf-8
-"""
-@version: 1.0
-@author: jin.dai
-@license: Apache Licence
-@contact: daijing491@gmail.com
-@software: PyCharm
-@file: Utils.py
-@time: 2017/7/13 15:45
-"""
-
 import functools
+import tensorflow as tf
 
-def lazy_property(function):
+def doublewrap(function):
+    """
+    A decorator decorator, allowing to use the decorator to be used without
+    parentheses if not arguments are provided. All arguments must be optional.
+    """
+    @functools.wraps(function)
+    def decorator(*args, **kwargs):
+        if len(args) == 1 and len(kwargs) == 0 and callable(args[0]):
+            return function(args[0])
+        else:
+            return lambda wrapee: function(wrapee, *args, **kwargs)
+    return decorator
+
+
+@doublewrap
+def define_scope(function, scope=None, *args, **kwargs):
+    """
+    A decorator for functions that define TensorFlow operations. The wrapped
+    function will only be executed once. Subsequent calls to it will directly
+    return the result so that operations are added to the graph only once.
+    The operations added by the function live within a tf.variable_scope(). If
+    this decorator is used with arguments, they will be forwarded to the
+    variable scope. The scope name defaults to the name of the wrapped
+    function.
+    """
     attribute = '_cache_' + function.__name__
+    name = scope or function.__name__
 
     @property
     @functools.wraps(function)
     def decorator(self):
         if not hasattr(self, attribute):
-            setattr(self, attribute, function(self))
+            with tf.variable_scope(name, *args, **kwargs):
+                setattr(self, attribute, function(self))
         return getattr(self, attribute)
-
     return decorator
