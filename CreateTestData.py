47b60f3d28b5ef6205247a4bcc9b869038a4997f
JIN-DAI <daijing491@gmail.com>
2017-07-14 17:42:51 +0200

    Modified comments

--- CreateTestData.py
+++ CreateTestData.py
@@ -8,7 +8,7 @@
 @software: PyCharm
 @file: CreateTestData.py
 @time: 2017/7/14 16:44
-@description:
+@description: function to create test sequence data for training and testing
 """
 
 import numpy as np
@@ -16,13 +16,13 @@ import matplotlib.pyplot as plt
 
 # function to create test input and output sequence with variable length
 def get_batch(timeSteps, batchSize, batchSTART, plotSwitch=False):
-    # xs shape (BATCH_SIZE, timeSteps)
+    # xs shape (batchSize, timeSteps)
     xs = np.arange(batchSTART, batchSTART+timeSteps*batchSize).reshape((batchSize, timeSteps)) / 10 * np.pi
     xs = xs[:,:,np.newaxis]
 
-    # input (BATCH_SIZE, timeSteps, input_size)
+    # input (batchSize, timeSteps, input_size)
     seq = np.append(np.sin(xs), np.cos(xs), axis=2)
-    # output (BATCH_SIZE, timeSteps, output_size)
+    # output (batchSize, timeSteps, output_size)
     res = np.append(np.sin(3*xs)**2+3*np.cos(xs+np.pi/6.0)**3, np.tanh(3*np.sin(xs)+np.cos(xs+np.pi/3)), axis=2)
 
     # plot res and seq in the first batch
@@ -34,5 +34,7 @@ def get_batch(timeSteps, batchSize, batchSTART, plotSwitch=False):
         plt.plot(xs[0,:], res[0,:,0], 'r', xs[0,:], res[0,:,1], 'b--')
         plt.ylabel('output')
         plt.show()
-    # return seq, res and shape (size, step, input)
+    # return seq (batchSize, timeSteps, input_size)
+    # return res (batchSize, timeSteps, output_size)
+    # return xs  (batchSize, timeSteps)
     return [seq, res, xs]
