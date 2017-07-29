b82fe682a5219d8f686c109da7cf1548d73e67ef
JIN-DAI <daijing491@gmail.com>
2017-07-30 06:57:51 +0800

    adjust configuration for training

--- Configs.py
+++ Configs.py
@@ -14,34 +14,34 @@
 class Config:
     def __init__(self):
         # Hyperparameters
-        self.BATCH_SIZE = 50  ## number of proteins
-        self.LR = 0.006  ## learning rate
+        self.BATCH_SIZE = 50  # number of proteins
+        self.LR = 0.006  # learning rate
         # Network Parameters
-        self.MAX_STEPS = 200  ## number of residues
-        self.INPUT_SIZE = 2   ## number of features for input, such as MSA information ...
-        self.OUTPUT_SIZE = 2  ## number of labels for output, such as Ramachandran angles ...
-        self.CELL_SIZE = 64   ## size of cell
+        self.MAX_STEPS = 200  # number of residues
+        self.INPUT_SIZE = 2   # number of features for input, such as MSA information ...
+        self.OUTPUT_SIZE = 2  # number of labels for output, such as Ramachandran angles ...
+        self.CELL_SIZE = 64   # size of cell
 
 
 class RamachandranConfig:
     def __init__(self):
         # Hyperparameters
-        self.BATCH_SIZE = 10  ## number of proteins
-        self.LR = 0.008  ## learning rate
+        self.BATCH_SIZE = 10  # number of proteins
+        self.LR = 0.008  # learning rate
         # Network Parameters
-        self.MAX_STEPS = 500  ## number of residues
-        self.INPUT_SIZE = 25   ## number of features for input, such as MSA information ...
-        self.OUTPUT_SIZE = 3  ## number of labels for output, such as Ramachandran angles ...
-        self.CELL_SIZE = 128   ## size of cell
+        self.MAX_STEPS = 500  # number of residues
+        self.INPUT_SIZE = 25   # number of features for input, such as MSA information ...
+        self.OUTPUT_SIZE = 3  # number of labels for output, such as Ramachandran angles ...
+        self.CELL_SIZE = 32   # size of cell
 
 
 class FrenetConfig:
     def __init__(self):
         # Hyperparameters
-        self.BATCH_SIZE = 10  ## number of proteins
-        self.LR = 0.008  ## learning rate
+        self.BATCH_SIZE = 10  # number of proteins
+        self.LR = 0.008  # learning rate
         # Network Parameters
-        self.MAX_STEPS = 500  ## number of residues
-        self.INPUT_SIZE = 25   ## number of features for input, such as MSA information ...
-        self.OUTPUT_SIZE = 2  ## number of labels for output, such as Ramachandran angles ...
-        self.CELL_SIZE = 128   ## size of cell
+        self.MAX_STEPS = 500  # number of residues
+        self.INPUT_SIZE = 25   # number of features for input, such as MSA information ...
+        self.OUTPUT_SIZE = 2  # number of labels for output, such as Ramachandran angles ...
+        self.CELL_SIZE = 32   # size of cell
